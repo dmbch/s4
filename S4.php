cd35d1086d6cbb0b8bece957f1475654fb65af39
dmbch <daniel@dmbch.net>
2014-03-16 23:28:16 +0100

    initial commit

---
+++ S4.php
@@ -0,0 +1,165 @@
+<?php
+
+class S4
+{
+  const ENDPOINT_TEMPLATE     = 'https://@host.amazonaws.com';
+
+  const REGION_AUSTRALIA      = 'ap-southeast-2';
+  const REGION_BRAZIL         = 'sa-east-1';
+  const REGION_CALIFORNIA     = 'us-west-1';
+  const REGION_IRELAND        = 'eu-west-1';
+  const REGION_JAPAN          = 'ap-northeast-1';
+  const REGION_OREGON         = 'us-west-2';
+  const REGION_SINGAPORE      = 'ap-southeast-1';
+  const REGION_VIRGINIA       = 'us-east-1';
+
+  const ACL_PRIVATE           = 'private';
+  const ACL_PUBLIC_READ       = 'public-read';
+  const ACL_PUBLIC_FULL       = 'public-read-write';
+  const ACL_AUTH_READ         = 'authenticated-read';
+  const ACL_OWNER_READ        = 'bucket-owner-read';
+  const ACL_OWNER_FULL        = 'bucket-owner-full-control';
+  const ACL_LOG_WRITE         = 'log-delivery-write';
+
+  const HEADER_SHA256         = 'x-amz-content-sha256';
+  const HEADER_STORAGE_CLASS  = 'x-amz-storage-class';
+  const HEADER_ENCRYPTION     = 'x-amz-server-side-encryption';
+
+  const STORAGE_STANDARD      = 'STANDARD';
+  const STORAGE_REDUCED       = 'REDUCED_REDUNDANCY';
+
+  const ENCRYPTION_AES        = 'AES256';
+
+
+  protected $accessKey;
+
+  protected $secretKey;
+
+  protected $bucket;
+
+  protected $region;
+
+  protected $endpoint;
+
+
+  public function __construct($accessKey, $secretKey, $bucket = null, $region = self::REGION_VIRGINIA)
+  {
+    $this->accessKey = $accessKey;
+    $this->secretKey = $secretKey;
+    $this->bucket = $bucket;
+    $this->region = $region;
+
+    $host = ($region === self::REGION_VIRGINIA) ? 's3' : "s3-$region";
+
+    $this->endpoint = str_replace('@host', $host, static::ENDPOINT_TEMPLATE);
+  }
+
+
+  public function request($method = 'GET', $path = '/', $file = null, $headers = array(), $options = array())
+  {
+    $path = '/'. ltrim($path, '/');
+    $url = $this->endpoint . $path;
+    $hash = (is_null($file)) ? hash('sha256', '') : hash_file('sha256', $file);
+
+    $headers = array_replace(
+      array(
+        'Date'                => gmdate('D, d M Y H:i:s \G\M\T'),
+        'Host'                => parse_url($url, PHP_URL_HOST),
+        static::HEADER_SHA256 => $hash
+      ),
+      $headers
+    );
+    ksort($headers);
+    $headers['Authorization'] = $this->sign($method, $path, $headers);
+
+    $formatted = array();
+    foreach ($headers as $key => $value) {
+      $formatted[] = "$key: $value";
+    }
+
+    $options = array_replace(
+      array(
+        CURLOPT_URL           => $url,
+        CURLOPT_CUSTOMREQUEST => $method,
+        CURLOPT_HTTPHEADER    => $formatted
+      ),
+      $options
+    );
+
+    return $this->curl($options);
+  }
+
+
+  protected function sign($method, $path, $headers)
+  {
+    $date       = gmdate('Ymd', strtotime($headers['Date']));
+    $query      = parse_url("$this->endpoint/$path", PHP_URL_QUERY);
+
+    $canonical = array();
+    foreach ($headers as $key => $value) {
+      $canonical[] = sprintf('%s:%s', strtolower($key), trim($value));
+    }
+    $canonical  = implode("\n", $canonical);
+    $signed     = implode(';', array_map('strtolower', array_keys($headers)));
+
+    $request    = sprintf(
+      "%s\n%s\n%s\n%s\n\n%s\n%s",
+      $method, $path, $query, $canonical, $signed, $headers[static::HEADER_SHA256]
+    );
+    $checksum   = hash('sha256', $request);
+
+    $scope      = sprintf('%s/%s/s3/aws4_request', $date, $this->region);
+    $string     = sprintf("AWS4-HMAC-SHA256\n%s\n%s\n%s", $headers['Date'], $scope, $checksum);
+    $key        = $this->keygen($date);
+
+    $signature  = hash_hmac('sha256', $string, $key);
+
+    return sprintf(
+      'AWS4-HMAC-SHA256 Credential=%s/%s,SignedHeaders=%s,Signature=%s',
+      $this->accessKey, $scope, $signed, $signature
+    );
+  }
+
+
+  protected function keygen($date)
+  {
+    $dateKey    = hash_hmac('sha256', $date, "AWS4$this->secretKey", true);
+    $regionKey  = hash_hmac('sha256', $this->region, $dateKey, true);
+    $serviceKey = hash_hmac('sha256', 's3', $regionKey, true);
+    $signingKey = hash_hmac('sha256', 'aws4_request', $serviceKey, true);
+
+    return $signingKey;
+  }
+
+
+  /**
+   * @param array $options
+   * @return array
+   */
+  protected function curl($options)
+  {
+    // obtain curl handle
+    $handle = curl_init();
+
+    // configure curl
+    curl_setopt_array($handle, array_replace(
+      array(
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => true,
+        CURLOPT_FOLLOWLOCATION => true
+      ),
+      $options
+    ));
+
+    // perform post request, gather response data
+    $result = curl_exec($handle);
+    $error = curl_error($handle);
+    $info = curl_getinfo($handle);
+
+    // close curl handle
+    curl_close($handle);
+
+    // process response data
+    return array_merge($info, compact('result', 'error'));
+  }
+}
